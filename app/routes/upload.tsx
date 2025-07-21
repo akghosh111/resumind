c232c38462acaf736defadd1c960294538d46769
akghosh111 <ghoshkiran2017@gmail.com>
2025-07-21 21:15:18 +0530

    feat: added resume & upload

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -62,10 +62,11 @@ const Upload = () => {
         
 
         data.feedback = JSON.parse(feedbackText);
-        await kv.set(`resume${uuid}`, JSON.stringify(data));
+        await kv.set(`resume:${uuid}`, JSON.stringify(data));
 
         setStatusText("Analysis complete, redirecting ...");
         console.log(data);
+        navigate(`/resume/${uuid}`);
 
     }
 
